ce1306d1c6ee8ec4882695d5640f444a55f735f4
chndrwali <chndrwali@gmail.com>
2024-01-20 20:02:24 +0700

    add contributor

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -9,9 +9,20 @@ const contributors = {
       alt: 'Candra Wali Sanjaya',
       socials: {
         linkedin: 'https://www.linkedin.com/in/chndrwali/',
-        github: 'https://github.com/chndrwali',
         instagram: 'https://instagram.com/chndrwali',
         twitter: 'https://twitter.com/chndrwali',
+      },
+    },
+    {
+      id: 0,
+      name: 'Rizal Maulana Sanjaya',
+      title: '02032011036',
+      image: '/img/contributors/rizal.webp',
+      alt: 'Rizal Maulana Sanjaya',
+      socials: {
+        linkedin: 'https://www.linkedin.com/in/',
+        instagram: 'https://instagram.com/rrizlms',
+        twitter: 'https://twitter.com/',
       },
     },
   ],
